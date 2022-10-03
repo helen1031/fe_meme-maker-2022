d235c1e8ade7a2aec3274e44d2e3cfa9c00820de
helen1031 <32950301+helen1031@users.noreply.github.com>
2022-10-04 04:39:15 +0900

    canvas api moveTo and lineTo

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.rect(50,50,100,200);
-ctx.rect(250,250,100,200);
-ctx.rect(150,150,100,200);
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
+ctx.stroke();
 ctx.fill();
 
-ctx.beginPath();
-ctx.rect(350,350,100,200);
-ctx.fillStyle = "red";
-setTimeout(() => {
-  ctx.fill();
-}, 5000);
+
